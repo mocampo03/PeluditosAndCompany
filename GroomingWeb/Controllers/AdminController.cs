a723549af81712b6dec411e7df5b5742a233247c
Miguel Ocampo <mocampor99@gmail.com>
2026-03-12 17:05:39 -0600

    Implementacion del panel de compras parea admin

--- GroomingWeb/Controllers/AdminController.cs
+++ GroomingWeb/Controllers/AdminController.cs
@@ -4,8 +4,13 @@ namespace GroomingWeb.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Productos()
+        public IActionResult Productos() => View();
+
+        public IActionResult Compras() => View();
+
+        public IActionResult CompraDetalle(string id)
         {
+            ViewBag.CompraId = id;
             return View();
         }
     }
--- GroomingWeb/obj/Debug/net8.0/GroomingWeb.AssemblyInfo.cs
+++ GroomingWeb/obj/Debug/net8.0/GroomingWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroomingWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81538446d12174a7f66ba8af5d90fa4cac3cf45")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroomingWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroomingWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
